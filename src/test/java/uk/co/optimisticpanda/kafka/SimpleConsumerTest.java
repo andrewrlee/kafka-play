57df38bf7f06be99e7d1164016d4a514d40a5d30
plasma147 <plasma147@hotmail.com>
2016-05-29 13:28:24 +0100

    Adding simple single threaded consumer

--- src/test/java/uk/co/optimisticpanda/kafka/SimpleConsumerTest.java
+++ src/test/java/uk/co/optimisticpanda/kafka/SimpleConsumerTest.java
@@ -1,52 +1,38 @@
 package uk.co.optimisticpanda.kafka;
 
-import static java.util.Collections.singleton;
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.junit.Rule;
 import org.junit.Test;
 
+import uk.co.optimisticpanda.kafka.clients.SimpleConsumer;
 import uk.co.optimisticpanda.kafka.clients.Producer;
 import uk.co.optimisticpanda.kafka.fixture.KafkaFixture;
 
 public class SimpleConsumerTest {
 
 	@Rule
-	public KafkaFixture fixture = KafkaFixture.create();
+	public final KafkaFixture fixture = KafkaFixture.create();
 
 	@Test
-	public void testSomething() throws Exception {
+	public void checkProducingAndConsuming() throws Exception {
 		Producer producer = new Producer(fixture.getKafkaConnect(), "topic-1");
-
-		Properties props = new Properties();
-		props.put("bootstrap.servers", fixture.getKafkaConnect());
-	    props.put("group.id", "test");
-	    props.put("enable.auto.commit", "true");
-	    props.put("auto.commit.interval.ms", "1000");
-	    props.put("session.timeout.ms", "30000");
-	    
-		try (KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props, new StringDeserializer(), new StringDeserializer())) {
-
-			consumer.subscribe(singleton("topic-1"));
-
-			consumer.poll(1000);
-			
-			producer.sendMessage();
-			producer.sendMessage();
-			producer.sendMessage();
-
-			ConsumerRecords<String, String> records = consumer.poll(1000);
-			
+		
+		try (SimpleConsumer consumer = new SimpleConsumer(fixture.getKafkaConnect(), "topic-1")) {
 			List<String> receivedValues = new ArrayList<>();
-			records.forEach(record -> receivedValues.add(record.value()));
-			assertEquals(3, receivedValues.size());
+			
+			consumer.startListening(receivedValues::add);
+			
+			producer.sendMessage();
+			producer.sendMessage();
+			producer.sendMessage();
+
+			consumer.poll(receivedValues::add);
+			
+			assertThat(receivedValues).hasSize(3);
 		}
 	}
 }
